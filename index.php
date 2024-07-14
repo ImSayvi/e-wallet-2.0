bb5386ca47617698411491c5d22ff6f47f2b2c75
Say.vi <say.julia.vi@gmail.com>
2024-07-14 16:23:03 +0200

    home page styling

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="utf-8">
@@ -10,10 +10,10 @@
 </head>
 
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid bg-dark">
         <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <div class="col-auto col-md-3 col-xl-2 px-0">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 left-side">
                     <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <span class="fs-5 d-none d-sm-inline">E-WALLET 2.0</span>
                     </a>
@@ -52,20 +52,101 @@
             </div>
 
 
-            <div class="col py-3 pl-0 tab-nav ">
+            <div class="col py-3 pl-0 tab-nav">
 
-                <nav data-bs-theme="dark">
+                <nav>
                     <div class="nav nav-tabs nav-justified" id="nav-tab" role="tablist">
                         <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Główna</button>
                         <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Konfiguracja wydatków stałych</button>
                         <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Wypłata</button>
                     </div>
                 </nav>
-                <div class="tab-content" id="nav-tabContent" data-bs-theme="dark">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">...</div>
+                <div class="tab-content row-centered" id="nav-tabContent">
+
+                    <div class="tab-pane text-center fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+                        <div class="row g-0 justify-content main">
+                            <div class="col-lg leftovers">
+                                <p>masz do wydania <br></p>
+                                <h3 class="amount">300 <span>zł</span></h3>
+                            </div>
+                            <div class="buttons">
+                                <button type="button" class="btn btn-success">+</button>
+                                <button type="button" class="btn btn-danger">-</button>
+                            </div>
+                            <div class="offensive">
+                                chuj Ci w dupe, nie ma hajsu
+                            </div>
+                        </div>
+                        <div class="row history">
+                            <div class="col expenses">
+                                <table class="table table-striped">
+                                    <thead class="bg-danger">
+                                        <tr>
+                                            <th scope="col">Kwota</th>
+                                            <th scope="col">Na co</th>
+                                            <th scope="col">Kiedy</th>
+                                            <th scope="col">Ile zostało</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col income">
+                                <table class="table table-striped table-dark">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Kwota</th>
+                                            <th scope="col">Na co</th>
+                                            <th scope="col">Kiedy</th>
+                                            <th scope="col">Ile zostało</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                     <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">...</div>
                     <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">...</div>
-                    <div class="tab-pane fade" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab" tabindex="0">...</div>
+
                 </div>
             </div>
         </div>
